c6f91ad4a0146c243bf322ce61aa0fc612d4f279
trforkan <trforkan@gmail.com>
2022-06-22 23:20:48 +0600

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+
+  weatherApiBaseUrl:'https://community-open-weather-map.p.rapidapi.com/weather',
+
+  XRapidAPIHostName: 'X-RapidAPI-Host',
+  XRapidAPIHostValue: 'community-open-weather-map.p.rapidapi.com',
+
+  XRapidAPIKeyName: 'X-RapidAPI-Key',
+  XRapidAPIKeyValue: '1995c2f739msh2ba9c82781c33f8p10ef75jsn99ed2fb49663',
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  weatherApiBaseUrl: 'https://community-open-weather-map.p.rapidapi.com/weather',
+  XRapidAPIHostName: 'X-RapidAPI-Host',
+  XRapidAPIHostValue: 'community-open-weather-map.p.rapidapi.com',
+
+  XRapidAPIKeyName: 'X-RapidAPI-Key',
+  XRapidAPIKeyValue: '1995c2f739msh2ba9c82781c33f8p10ef75jsn99ed2fb49663' 
 };
 
 /*
